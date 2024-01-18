6d1649a730f19e0aa962b78ef1a09fc6a224487d
Hyunkyung-Nam <gusrud13579@gmail.com>
2024-01-18 12:24:12 +0900

    지도추천 페이지 세팅

---
+++ js/mapRecommand.js
@@ -0,0 +1,23 @@
+function resetMap() {
+    $('.content-container').removeClass('hide');
+}
+function map1Click() {
+    const contentCotainer = $('.content-container');
+    contentCotainer.addClass('hide');
+    $('.content-container.map1').removeClass('hide');
+}
+function map2Click() {
+    const contentCotainer = $('.content-container');
+    contentCotainer.addClass('hide');
+    $('.content-container.map2').removeClass('hide');
+}
+function map3Click() {
+    const contentCotainer = $('.content-container');
+    contentCotainer.addClass('hide');
+    $('.content-container.map3').removeClass('hide');
+}
+function map4Click() {
+    const contentCotainer = $('.content-container');
+    contentCotainer.addClass('hide');
+    $('.content-container.map4').removeClass('hide');
+}
