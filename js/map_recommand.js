6dbf47450917aa7c790290a2056a6c3bcc60d519
Hyunkyung-Nam <gusrud13579@gmail.com>
2024-01-20 16:36:59 +0900

    feat/지도 추천페이지 초기 세팅 및 전체보기 버튼 세팅, 컨텐츠 눌렀을 때 show_content화면 연결

--- js/map_recommand.js
+++ js/map_recommand.js
@@ -1,7 +1,15 @@
-import { drawMap } from './draw_map.js';
+import { drawMap, initSetting } from './draw_map.js';
 
 drawMap();
 
 $('.reset-map').click(function () {
-    $('.content-containe').removeClass('hide');
+    $('.content-container').removeClass('hide');
+    $('.page-numbering').empty();
+    initSetting();
+});
+
+$('.content-container').click(function () {
+    const contentName = $(this).children('div:last').children('.content-preview-header').text();
+    localStorage.setItem('name', contentName);
+    window.location.href = '../html/show_content.html';
 });
